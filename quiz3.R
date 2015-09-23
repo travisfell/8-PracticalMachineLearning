119970bd8dec21eb1de50e2fbbe53918d3affad2
travisfell <tfell2001@gmail.com>
2015-09-22 21:18:22 -0500

    updates through question 4

---
+++ quiz3.R
@@ -0,0 +1,65 @@
+#  quiz 3
+
+library(caret)
+library(AppliedPredictiveModeling)
+library(ElemStatLearn)
+library(pgmm)
+library(rpart)
+library(rattle)
+
+# question 1
+data(segmentationOriginal)
+head(segmentationOriginal)
+View(segmentationOriginal)
+str(segmentationOriginal)
+inTrain <- createDataPartition(y=segmentationOriginal$Case)
+
+#training <- segmentationOriginal[inTrain,]
+#testing <- segmentationOriginal[-inTrain,]
+
+training <- subset(segmentationOriginal, Case == 'Train')
+testing <- subset(segmentationOriginal, Case == 'Test')
+
+set.seed(125)
+modFit <- train(Class ~., method = "rpart", data = training)
+print(modFit$finalModel)
+plot(modFit$finalModel, uniform=TRUE, main="classification tree")
+text (modFit$finalModel, use.n=TRUE, all=TRUE, cex=.8)
+fancyRpartPlot(modFit$finalModel)
+
+# question 2
+
+
+# question 3
+library(pgmm)
+data(olive)
+olive = olive[,-1]
+
+inTrain <- createDataPartition(y = olive$Area, p = .7, list = FALSE)
+training <- olive[inTrain,]
+testing <- olive[-inTrain,]
+newdata = as.data.frame(t(colMeans(olive)))
+modFit <- train(Area ~., method = "rpart", data=training)
+print(modFit$finalModel)
+predict(modFit, newdata = newdata)
+
+#  question 4
+library(ElemStatLearn)
+data(SAheart)
+set.seed(8484)
+train = sample(1:dim(SAheart)[1],size=dim(SAheart)[1]/2,replace=F)
+trainSA = SAheart[train,]
+testSA = SAheart[-train,]
+set.seed(13234)
+q4fit <- glm(chd ~ age + alcohol + obesity + tobacco + typea + ldl, data = trainSA)
+missClass = function(values,prediction){sum(((prediction > 0.5)*1) != values)/length(values)}
+missClass(trainSA, q4fit)
+
+#question 5
+library(ElemStatLearn)
+data(vowel.train)
+data(vowel.test) 
+set.seed(33833)
+vowel.test
+vowel.test$y <- as.factor(vowel.test$y)
+vowel.train$y <- as.factor(vowel.train$y)
